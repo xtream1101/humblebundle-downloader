ad053fa941acc31d4610f444176f96f411e6dd36
TechnoSwiss <trex@taipan.themeadows.local>
2020-05-14 23:40:36 -0700

    Fix bundle_title_parts to use split instead of rsplit Add several more allowed characters +, (, ), ' Changed/Added several replacements / to -, * to -, " to ', < to (, > to ), | to -, and modifications to replacements to : so spacing looks right

--- humblebundle_downloader/download_library.py
+++ humblebundle_downloader/download_library.py
@@ -11,9 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 def _clean_name(dirty_str):
-    allowed_chars = (' ', '_', '.', '-', '[', ']', ',', '#', '&')
+    allowed_chars = (' ', '_', '.', '-', '+', '(', ')', '[', ']', '\'', ',', '#', '&')
     clean = []
-    for c in dirty_str.replace('+', '_').replace(':', ' -'):
+    for c in dirty_str.replace('/', '-').replace(' : ', ' - ').replace(': ', ' - ').replace(' :', ' - ').replace(':', '-').replace('*', '-').replace('"', '\'').replace('<', '(').replace('>', ')').replace('|', '-'):
         if c.isalpha() or c.isdigit() or c in allowed_chars:
             clean.append(c)
 
@@ -196,7 +196,7 @@ class DownloadLibrary:
 
         logger.debug("Order request: {order_r}".format(order_r=order_r))
         order = order_r.json()
-        bundle_title_parts = order['product']['human_name'].rsplit(': ',1)
+        bundle_title_parts = order['product']['human_name'].split(': ',1)
         bundle_title = _clean_name(bundle_title_parts[0])
         bundle_title_sub = _clean_name(bundle_title_parts[1]) if self.sub_title and (":" in order['product']['human_name']) else ""
         logger.info("Checking bundle: " + str(bundle_title))
